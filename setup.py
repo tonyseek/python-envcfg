c227710cff415e0e3f2fa473111c47b7d4f2bed6
Jiangge Zhang <tonyseek@gmail.com>
2014-09-07 23:59:27 +0800

    supply the pypi classifers.

--- setup.py
+++ setup.py
@@ -19,9 +19,12 @@ setup(
     packages=find_packages(),
     classifiers=[
         'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries',
     ]
 )
